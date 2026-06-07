b2b5cada6d3986ad34c8013505b96b2b1e727e9f
Srimanchal Sahoo <srisahoo.testing@gmail.com>
2026-06-08 00:43:40 +0530

    Add db test

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,29 @@
 from flask import Flask
 import os
+import pymysql
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello from Flask on Render!"
+
+@app.route("/dbtest")
+def dbtest():
+    try:
+        conn = pymysql.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+
+        conn.close()
+
+        return "Database Connected Successfully!"
+
+    except Exception as e:
+        return f"Database Error: {str(e)}"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
